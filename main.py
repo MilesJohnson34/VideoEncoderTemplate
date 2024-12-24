6292704543231e68482949c69bafbdb327a96acf
MilesJohnson34 <milesivyjohnson@gmail.com>
2024-12-24 23:17:48 +0000

    piggyvid

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import encoder 
 
 def main():
-    encoder.encode("TheaterIntro","TheaterIntro.mp4",.1,.7,10)
+    encoder.encode("NightBeforePigmas","Twas_The_Night_Before_Pigmas.mp4",.1,.69,10)
 
 if __name__== "__main__":
     main()
