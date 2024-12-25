278c56689a1623e6f0015d473e8f4bec86a7a856
MilesJohnson34 <milesivyjohnson@gmail.com>
2024-12-25 01:07:09 +0000

    fixattempt?

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import encoder 
 
 def main():
-    encoder.encode("NightBeforePigmas","Twas_The_Night_Before_Pigmas.mp4",.35,.8,10)
+    encoder.encode("NightBeforePigmas","Twas_The_Night_Before_Pigmas.mp4",.3,.7,10)
 
 if __name__== "__main__":
     main()
