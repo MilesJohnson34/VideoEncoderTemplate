950145e8762597918676e27180a44f20487bb69a
MilesJohnson34 <milesivyjohnson@gmail.com>
2024-12-26 22:06:39 +0000

    SonicScene

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import encoder 
 
 def main():
-    encoder.encode("NightmareBeforePigmas","Twas_The_Night_Before_Pigmas.mp4",.3,.7,10)
+    encoder.encode("sonic3scene","sonic3scene.mp4",.1,0.85,10)
 
 if __name__== "__main__":
     main()
