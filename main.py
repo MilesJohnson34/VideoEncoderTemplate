4187ea5dd8d0bbe4a76269190f48667339e0aad2
MilesJohnson34 <milesivyjohnson@gmail.com>
2024-12-27 22:54:31 -0800

    if this doesnt work ima crash out

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import encoder 
 
 def main():
-    encoder.encode("NightBeforePigmas","Twas_The_Night_Before_Pigmas.mp4",.3,.7,10)
+    encoder.encode("seekchase","SeekMinesChase2.mp4",.15,.7,10)
 
 if __name__== "__main__":
     main()
